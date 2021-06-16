7f6fa5ba45969f60b77fd5d045228958d092d54c
arindam29 <arindamdutta1996@gmail.com>
2021-06-16 21:18:13 +0530

    Update archs.py

--- archs.py
+++ archs.py
@@ -25,44 +25,6 @@ class VGGBlock(nn.Module):
         return out
 
 
-class UNet(nn.Module):
-    def __init__(self, num_classes, input_channels=30, **kwargs):
-        super().__init__()
-
-        nb_filter = [32, 64, 128, 256, 512]
-
-        self.pool = nn.MaxPool2d(2, 2)
-        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-
-        self.conv0_0 = VGGBlock(input_channels, nb_filter[0], nb_filter[0])
-        self.conv1_0 = VGGBlock(nb_filter[0], nb_filter[1], nb_filter[1])
-        self.conv2_0 = VGGBlock(nb_filter[1], nb_filter[2], nb_filter[2])
-        self.conv3_0 = VGGBlock(nb_filter[2], nb_filter[3], nb_filter[3])
-        self.conv4_0 = VGGBlock(nb_filter[3], nb_filter[4], nb_filter[4])
-
-        self.conv3_1 = VGGBlock(nb_filter[3]+nb_filter[4], nb_filter[3], nb_filter[3])
-        self.conv2_2 = VGGBlock(nb_filter[2]+nb_filter[3], nb_filter[2], nb_filter[2])
-        self.conv1_3 = VGGBlock(nb_filter[1]+nb_filter[2], nb_filter[1], nb_filter[1])
-        self.conv0_4 = VGGBlock(nb_filter[0]+nb_filter[1], nb_filter[0], nb_filter[0])
-
-        self.final = nn.Conv2d(nb_filter[0], num_classes, kernel_size=1)
-
-
-    def forward(self, input):
-        x0_0 = self.conv0_0(input)
-        x1_0 = self.conv1_0(self.pool(x0_0))
-        x2_0 = self.conv2_0(self.pool(x1_0))
-        x3_0 = self.conv3_0(self.pool(x2_0))
-        x4_0 = self.conv4_0(self.pool(x3_0))
-
-        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
-        x2_2 = self.conv2_2(torch.cat([x2_0, self.up(x3_1)], 1))
-        x1_3 = self.conv1_3(torch.cat([x1_0, self.up(x2_2)], 1))
-        x0_4 = self.conv0_4(torch.cat([x0_0, self.up(x1_3)], 1))
-
-        output = self.final(x0_4)
-        return output
-
 
 class NestedUNet(nn.Module):
     def __init__(self, num_classes =1, input_channels=30, deep_supervision=False, **kwargs):
